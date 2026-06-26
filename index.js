8a935bc62c7bbc99a92572a467f336a522f9e3a8
AdityaKumar188 <adityaa.kumar.98909@gmail.com>
2026-06-26 12:54:33 +0530

    Added Delet and update

--- index.js
+++ index.js
@@ -11,10 +11,60 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("API is running");
 });
+// Data Retrieve
 app.get("/students",(req,res)=>{
     res.json({
         message:"All Students",
         data:students
+    });
+});
+// Data Store
+app.post("/students",(req,res)=>{
+    const {id,name,city}=req.body;
+    const newStudent={id,name,city};
+    students.push(newStudent);
+    res.json({
+        message:"Record Added",
+        student:newStudent,
+        data:students
+    });
+
+});
+
+// Data update
+app.put("/students/:id",(req,res)=>{
+    // First method
+    // const id =req.params.id;
+
+    // Second method
+    const {id}=req.params;
+    const student=students.find(s=>s.id==id);
+    if(!student)
+    {
+        return res.status(404).json({
+            message:"Student Not Found"
+        });
+    }
+    student.name=req.body.name;
+    student.city=req.body.city;
+    res.json({
+        message:"Record Updated",
+        student
+    });
+
+});
+// Data Delet
+app.delete("/students/:id",(req,res)=>{
+    const id=req.params.id;
+    const student=students.find(s=>s.id==id);
+    if(!student)
+    {
+        return reqs.status(404).json({message:"Invalid ID"});
+    }
+    students=students.filter(s=>s.id!=id);
+    res.json({
+        message:"Record deleted",
+        students
     });
 });
 
